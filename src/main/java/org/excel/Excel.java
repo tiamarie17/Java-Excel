7d92bfbd743df753f4f6a4afb9c04d622bd86e1b
Tia Theisen <Tia.Theisen@state.mn.us>
2023-03-29 13:15:29 -0500

    update folders and packages

--- src/main/java/org/excel/Excel.java
+++ src/main/java/org/excel/Excel.java
@@ -94,7 +94,6 @@ public class Excel {
 
             return hashMap;
 
-
     }
 
 
--- src/test/java/org/excel/ExcelPOIHelper.java
+++
@@ -1,6 +0,0 @@
-package org.excel;
-
-public interface ExcelPOIHelper {
-    void writeExcel();
-    void readExcel();
-}
