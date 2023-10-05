8cdfbaac9633c9cc07112f8110395786dfbb598d
Tia Theisen <Tia.Theisen@state.mn.us>
2023-10-05 11:14:51 -0500

    update code comments

--- src/main/java/org/excel/Excel.java
+++ src/main/java/org/excel/Excel.java
@@ -171,7 +171,6 @@ public class Excel {
         return workbook;
     }
 
-
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("words.txt");
         //Open Excel document and grab first sheet
@@ -196,37 +195,6 @@ public class Excel {
     }
 }
 
-
-
-//TEST
-//        public class ExcelIntegrationTest {
-//
-//            private ExcelPOIHelper excelPOIHelper;
-//            private static String FILE_NAME = "temp.xlsx";
-//            private String fileLocation;
-//
-//            @Before
-//            public void generateExcelFile() throws IOException {
-//                File currDir = new File(".");
-//                String path = currDir.getAbsolutePath();
-//                fileLocation = path.substring(0, path.length() - 1) + FILE_NAME;
-//
-//                excelPOIHelper = new ExcelPOIHelper();
-//                excelPOIHelper.writeExcel();
-//            }
-//
-//            @Test
-//            public void whenParsingPOIExcelFile_thenCorrect() throws IOException {
-//                Map<Integer, List<String>> data
-//                        = excelPOIHelper.readExcel(fileLocation);
-//
-//                assertEquals("Word", data.get(0).get(0));
-//                assertEquals("Count", data.get(0).get(1));
-//
-//                assertEquals("climate", data.get(1).get(0));
-//                assertEquals("14", data.get(1).get(1));
-//            }
-//        }
 
 
 
